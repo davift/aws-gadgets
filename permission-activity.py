95a43ec2cfbad3851f2558844bdde3a41968af6c
Davi Torres <73488542+davift@users.noreply.github.com>
2024-10-27 21:14:53 -0400

    Add files via upload

---
+++ permission-activity.py
@@ -0,0 +1,77 @@
+import boto3
+import datetime
+import json
+import sys
+from tabulate import tabulate
+
+username=sys.argv[1]
+window=sys.argv[2]
+
+blue='\033[94m'
+green='\033[92m'
+red='\033[91m'
+reset='\033[0m'
+
+print('')
+print(tabulate([[blue + 'CloudTrail Access Analysis' + reset]], tablefmt="simple"))
+
+client = boto3.client('cloudtrail')
+
+events = []
+next_token = None
+params = {
+    'LookupAttributes': [
+        {
+            'AttributeKey': 'Username',
+            'AttributeValue': username
+        },
+    ],
+    'StartTime': datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=int(window)),
+    'EndTime': datetime.datetime.now(datetime.timezone.utc),
+    'MaxResults': 50
+}
+while True:
+    try:
+        response = client.lookup_events(**params)
+        events.extend(response['Events'])
+        next_token = response.get('NextToken')
+        if not next_token:
+            break
+        else:
+            params['NextToken'] = next_token
+    except Exception as e:
+        print(f"Error retrieving events: {e}")
+        break
+
+errors = []
+grants = []
+if not events:
+    print('')
+    print("No events found for the specified user.")
+    print('')
+    exit()
+else:
+    for event in events:
+        log_data = json.loads(event['CloudTrailEvent'])
+        #print(log_data)
+        if 'errorCode' in log_data:
+            errors.append(log_data['eventName'])
+        else:
+            grants.append(log_data['eventName'])
+
+print('')
+print('User:', username)
+print('Activity: last', window, 'hours')
+print('')
+print(tabulate([["Total events found", blue + str(len(events)) + reset], ["Events with errors", red + str(len(errors)) + reset], ["Events with grants", green + str(len(grants)) + reset]], tablefmt="simple"))
+
+if len(errors) > 0:
+    print('')
+    print(tabulate([[error] for error in list(set(errors))], headers=[red + "Denied Permission(s)" + reset], tablefmt="firstrow"))
+
+if len(grants) > 0:
+    print('')
+    print(tabulate([[grant] for grant in list(set(grants))], headers=[green + "Granted Permission(s)" + reset], tablefmt="firstrow"))
+
+print('')
+exit()
